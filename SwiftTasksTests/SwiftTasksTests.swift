fa05525d0f61fcd2266611c369683652d10bf8cb
Jonathan Steele <iNoles@users.noreply.github.com>
2024-09-12 18:47:24 -0400

    Initial Commit

---
+++ SwiftTasksTests/SwiftTasksTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTasksTests.swift
+//  SwiftTasksTests
+//
+//  Created by Jonathan Steele on 9/12/24.
+//
+
+import Testing
+@testable import SwiftTasks
+
+struct SwiftTasksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
